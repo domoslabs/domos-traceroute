b964fc2e20ce03791dae246985a2e1935ee24379
vladim0105 <vladim0105@gmail.com>
2021-09-01 11:47:01 +0200

    Modernized argument parsing, and fixed very minor bugs.

--- README.md
+++ README.md
@@ -1,27 +1,38 @@
 # Domos Traceroute
-A traceroute by Domos which can control per flow ECMP, and that way map paths to an endpoint. TCP only for now.
+
+A traceroute by Domos which can reliably detect multiple paths by carefully modifying the packet headers of the probes.
+
 ## Installation
+
 Clone the repo and all its submodules:
+
 ```bash
 git clone --recurse-submodules https://github.com/domoslabs/domos-traceroute.git 
 ```
+
 Then using cmake:
+
 ```bash
 mkdir build
 cd build
 cmake ..
 make
 ```
+
 This outputs an executable.
 
 Optional:
+
 ```bash
 make install
 ```
+
 ## Usage
+
 Requires root access.
 
 To show the help message:
+
 ```bash
 ./domos-traceroute -h
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,11 @@
 #include <fstream>
 #include <getopt.h>
 #include "effolkronium/random.hpp"
+#include "argparse/argparse.hpp"
 
 using Random = effolkronium::random_static;
-const char *target = nullptr;
+// Default values
+std::string target;
 ProbeType probeType = ProbeType::TCP;
 uint16_t baseSrcPort = Random::get<uint16_t>(33000, 40000);
 uint16_t dstPort = 80;
@@ -20,128 +22,101 @@ uint16_t max_ttl = 15;
 uint32_t n_runs = 3;
 uint32_t interval_delay = 50;
 uint32_t timeout_delay = 500;
-const char *interface = nullptr;
-const char *file = nullptr;
+std::string interface;
+std::string file;
 bool compress = false;
+bool udp = false;
 pcpp::PcapLiveDevice *device;
 
-void show_help(char *progname) {
-    std::cout
-            << "\nA traceroute by Domos which can control per flow ECMP, and that way map paths to an endpoint. TCP only for now. \n"
-            << std::endl;
-    std::cout << "Usage: " << progname
-              << " <target_host> [--sport] [--dport] [--ttl] [--n_paths] [--n_runs] [--interface] [--file] [--help]"
-              << std::endl;
-    std::cout << "target_host                     The hostname or IP of the target host" << std::endl;
-    std::cout
-            << "-s --sport                      A port which will define source port range used: [sport, sport+n_paths] Default is random in range [33000, 40000]"
-            << std::endl;
-    std::cout
-            << "-d --dport                      The target destination port. For TCP, a good port is 80. For UDP a good port is 33434. Default is ("
-            << dstPort << ")" << std::endl;
-    std::cout << "-u --udp                        Use UDP probes instead. Uses TCP by default." << std::endl;
-    std::cout << "-t --ttl                        The time-to-live value to count up to. Default is (" << max_ttl << ")"
-              << std::endl;
-    std::cout << "-p --n_paths                    Amount of paths to probe. Default is (" << n_paths << ")"
-              << std::endl;
-    std::cout << "-n --n_runs                     Amount of runs to perform. Default is (" << n_runs << ")"
-              << std::endl;
-    std::cout << "-I --interval                   Interval between probes (ms). Default is (" << interval_delay << ")"
-              << std::endl;
-    std::cout << "-T --timeout                    How long to wait for probes to return (ms). Default is ("
-              << timeout_delay << ")" << std::endl;
-    std::cout
-            << "-i --interface                  The interface to use, given by name or IP. Finds and uses a interface with a default gateway by default."
-            << std::endl;
-    std::cout << "-f --file                       File name to save the results to. Optional." << std::endl;
-    std::cout << "-c --compress                   Whether or not to compress the output file as bzip2. Optional."
-              << std::endl;
-    std::cout << "-h --help                       Show this message." << std::endl;
-}
 
 void parse_args(int argc, char **argv) {
-    const char *shortopts = "s:d:ut:p:n:I:T:i:f:ch";
-    const struct option longopts[] = {
-            {"sport",     required_argument, 0, 's'},
-            {"dport",     required_argument, 0, 'd'},
-            {"udp",       no_argument,       0, 'u'},
-            {"ttl",       required_argument, 0, 't'},
-            {"n_paths",   required_argument, 0, 'p'},
-            {"n_runs",    required_argument, 0, 'n'},
-            {"interval",  required_argument, 0, 'I'},
-            {"timeout",   required_argument, 0, 'T'},
-            {"interface", required_argument, 0, 'i'},
-            {"file",      required_argument, 0, 'f'},
-            {"compress",  no_argument,       0, 'c'},
-            {"help",      no_argument,       0, 'h'},
-            {0, 0,                           0, 0},
-    };
-    int c, option_index;
-    while ((c = getopt_long(argc, argv, shortopts, longopts, &option_index)) != -1)
-        switch (c) {
-            case 's':
-                baseSrcPort = std::stoul(optarg);
-                break;
-            case 'd':
-                dstPort = std::stoul(optarg);
-                break;
-            case 'u':
-                probeType = ProbeType::UDP;
-                break;
-            case 't':
-                max_ttl = std::stoul(optarg);
-                break;
-            case 'p':
-                n_paths = std::stoul(optarg);
-                break;
-            case 'n':
-                n_runs = std::stoul(optarg);
-                break;
-            case 'I':
-                interval_delay = std::stoul(optarg);
-                break;
-            case 'T':
-                timeout_delay = std::stoul(optarg);
-                break;
-            case 'i':
-                interface = optarg;
-                break;
-            case 'f':
-                file = optarg;
-                break;
-            case 'c':
-                compress = true;
-                break;
-            case 'h':
-                show_help(argv[0]);
-                std::exit(EXIT_SUCCESS);
-            default:
-                std::cerr << "Invalid argument: " << c << ". See --help." << std::endl;
-                std::exit(EXIT_FAILURE);
-        }
-    if (optind == argc) {
-        std::cerr << "Missing target. See --help." << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-    target = argv[optind];
-}
+    argparse::ArgumentParser program("Domos Traceroute");
+    program.add_argument("target_host")
+            .help("The hostname or IP of the target host.");
+    program.add_argument("-s", "--sport")
+            .help("A port which will define source port range used: [sport, sport+n_paths] Default is random in range [33000, 40000].")
+            .scan<'i', uint16_t>()
+            .default_value(baseSrcPort);
+    program.add_argument("-d", "--dport")
+            .help("The target destination port. For TCP, a good port is 80. For UDP a good port is 33434.")
+            .scan<'i', uint16_t>()
+            .default_value(dstPort);
+    program.add_argument("-u", "--udp")
+            .help("Use UDP probes instead.")
+            .default_value(udp)
+            .implicit_value(!udp);
+    program.add_argument("-t", "--ttl")
+            .help("The time-to-live value to count up to.")
+            .scan<'i', uint16_t>()
+            .default_value(max_ttl);
+    program.add_argument("-p", "--paths")
+            .help("Amount of paths to probe.")
+            .scan<'i', uint16_t>()
+            .default_value(n_paths);
+    program.add_argument("-n", "--n_runs")
+            .help("Amount of runs to perform.")
+            .scan<'i', uint32_t>()
+            .default_value(n_runs);
+    program.add_argument("-I", "--interval")
+            .help("Interval between probes (ms).")
+            .scan<'i', uint32_t>()
+            .default_value(interval_delay);
+    program.add_argument("-T", "--timeout")
+            .help("How long to wait for probes to return (ms).")
+            .scan<'i', uint32_t>()
+            .default_value(timeout_delay);
+    program.add_argument("-i", "--interface")
+            .help("The interface to use, given by name or IP. Finds and uses a interface with a default gateway by default.")
+            .default_value(interface);
+    program.add_argument("-f", "--file")
+            .help("File name to save the results to. Optional.")
+            .default_value(file);
+    program.add_argument("-c", "--compress")
+            .help(" Whether or not to compress the output file as bzip2. Optional.")
+            .default_value(compress)
+            .implicit_value(!compress);
 
-int main(int argc, char *argv[]) {
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err) {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+    target = program.get("target_host");
+    baseSrcPort = program.get<uint16_t>("--sport");
+    dstPort = program.get<uint16_t>("--dport");
+    n_paths = program.get<uint16_t>("--paths");
+    max_ttl = program.get<uint16_t>("--ttl");
+    n_runs = program.get<uint32_t>("--n_runs");
+    interval_delay = program.get<uint32_t>("--interval");
+    timeout_delay = program.get<uint32_t>("--timeout");
+    interface = program.get("--interface");
+    file = program.get("--file");
+    compress = program.get<bool>("--compress");
+    if (program["--udp"] == true) {
+        probeType = ProbeType::UDP;
+    }
+
     if (getuid() != 0) {
         std::cerr << "Insufficient privileges, please run as root." << std::endl;
         std::exit(EXIT_FAILURE);
     }
+}
+
+int main(int argc, char *argv[]) {
     parse_args(argc, argv);
     std::cout << "Status: Resolving...\r" << std::flush;
-    if (interface == nullptr) {
+    if (interface.empty()) {
         device = findDefaultDevice();
     } else {
         device = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByIpOrName(interface);
     }
     pcpp::MacAddress gatewayMac = getGatewayMac(device);
-    pcpp::IPv4Address targetIp = pcpp::IPv4Address(target);
+    auto targetIp = pcpp::IPv4Address(target);
     if (!targetIp.isValid()) {
-        targetIp = getHostNameIpAddress(target);
+        targetIp = getHostNameIpAddress(target.c_str());
         if (gatewayMac == pcpp::MacAddress::Zero || targetIp == pcpp::IPv4Address::Zero) {
             throw std::runtime_error("Could not resolve gateway mac or target ip.");
         }
@@ -183,7 +158,7 @@ int main(int argc, char *argv[]) {
     // Create json
     std::string out = tr->to_json();
     // Write to file if file has been defined, otherwise write to terminal.
-    if (file != nullptr) {
+    if (!file.empty()) {
         if (compress) {
             std::string compressedFile = file;
             compressedFile += ".bz2";
